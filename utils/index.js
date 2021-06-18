d6fc9979d56fff9d447ad6cae56e69c2c331b97b
Uberpoch <nathan.poch@uberflip.com>
2021-06-17 22:19:28 -0400

    next push and remove env

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,24 @@
+const h = require('./helpers');
+const dbs = require('./db-scripts');
+const zds = require('./zd-scripts');
+const ufs = require('./uf-scripts');
+const sort = require('./sort');
+
+const zdCreds = Buffer.from(process.env.ZD_STRING).toString('base64');
+
+
 const run = async () => {
   console.log('the first one is working?');
+  try {
+  
+  
+  
+  
+  } catch (err) {
+    
+  }
+  
 }
-setTimeout(run, 1000);
+setTimeout(run, 500);
 // run();
 module.exports = run;
